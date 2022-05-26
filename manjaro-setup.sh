0f61d2b9f4d56e6b4105af28383319bf39e17f77
Almaz Yessenyazov <mr.esenyazov@gmail.com>
2022-05-26 15:03:14 +0600

    Added poetry and redis

--- manjaro-setup.sh
+++ manjaro-setup.sh
@@ -9,11 +9,19 @@ ssh-keygen
 
 # Install git
 echo "Installing packages"
-sudo pacman -S --needed base-devel git gnome-tweaks snapd
+sudo pacman -S --needed base-devel git gnome-tweaks snapd networkmanager-openvpn docker docker-compose
+
+# Enable docker service
+echo "Enabling docker service"
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+
+# Run docker without root
+sudo usermod -aG docker $USER
 
 # Enable snapd
 echo "Enable snapd package manager"
-sudo systemctl enable --now snapd.socket
+sudo systemctl enable --now  snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 
 # Configure git
@@ -41,3 +49,14 @@ sudo snap install telegram-desktop
 
 echo "Installing Postman"
 sudo snap install postman
+
+echo "Installing dbeaver"
+sudo snap install dbeaver-ce
+
+echo "Installing Redis Desktop manager"
+sudo snap install redis-desktop-manager
+
+# Install Poetry
+echo "Installing Poetry"
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+source $HOME/.poetry/env
